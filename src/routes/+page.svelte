4ec195f73cb854d8e8ae0125c35a23ae328a61f5
kolodiva <kolodiva@mail.ru>
2022-12-30 13:55:43 +0300

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
   // import {data1} from "../stores/data";
 
   // console.log(data1);
-  //export let data;
-  //console.log(data)
+  // export let data;
+  // console.log(data)
 </script>
 
 <svelte:head>
@@ -14,7 +14,8 @@
 
 <!-- <Nav/> -->
 <div class="container mx-auto p-2">
-  <h1 class="text-2xl p-4 underline underline-offset-8 border-x-4 border-indigo-500 inline-block">Welcome to SvelteKit</h1>
+  <h1 class="text-2xl p-4 underline underline-offset-8 border-x-4 border-indigo-500 ">Welcome to SvelteKit</h1>
+  <!-- <p class="text-2xl p-4 underline underline-offset-8 border-x-4 border-indigo-500 ">{data}</p> -->
 </div>
 
 
