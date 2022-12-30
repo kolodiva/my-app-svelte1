2433850581cd79c69504a69d7683d4c872109aa1
kolodiva <kolodiva@mail.ru>
2022-12-30 06:47:59 +0300

    ok

--- src/routes/askartamon/+server.js
+++ src/routes/askartamon/+server.js
@@ -11,30 +11,34 @@ export const POST = async ({request}) => {
 
 	const msg = await request.json()
 
-	messageText = msg?.text?.toLowerCase()?.trim()
+	botMessage = msg?.text?.toLowerCase()?.trim()
 	chatId = msg?.chat?.id
 
 	// console.log('chatId');
 	// console.log(chatId);
 	// console.log(messageText);
 
-	if (!messageText || !chatId) {
+	if (!botMessage || !chatId) {
   		throw error(400, 'no data')
 	}
 
 	try {
-			const TELEGRAM_URI = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage`
 
-			const rsp = await json({
-			    chat_id: chatId,
-			    text: messageText
-			  });
+			// const rsp = await json({
+			//     chat_id: chatId,
+			//     text: messageText
+			//   });
 
 			//return json(chatId)
-			await axios.post(TELEGRAM_URI, rsp)
+			//await axios.post(TELEGRAM_URI, rsp)
 
-				return new Response( 'Done')
+				//return new Response( 'Done')
 
+				// const url = `https://api.telegram.org/bot${Token}/sendMessage?chat_id=${chatId}&text=${botMessage}`;
+				const TELEGRAM_URI = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage?chat_id=${chatId}&text=${botMessage}`
+
+        const res = await fetch(TELEGRAM_URI);
+        return await res.json();
 
 		} catch (message) {
 
