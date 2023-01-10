5d7db9bba686ff1999b87248275cb38784582c99
kolodiva <kolodiva@mail.ru>
2023-01-10 10:54:50 +0300

    ok

--- src/routes/askmfcinfo/+server.js
+++ src/routes/askmfcinfo/+server.js
@@ -51,7 +51,7 @@ export const POST = async ({request}) => {
 
 		const res = await axios.post(TELEGRAM_URI, {
 				 chat_id: chatId,
-				 text: JSON.stringify(response)
+				 text: response
 			 })
 
 		return new Response('Done')
