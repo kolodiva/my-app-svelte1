7fc8ef2dcf55b5e5baa9e1639ee53ca2f3020a3f
kolodiva <kolodiva@mail.ru>
2023-01-10 15:26:31 +0300

    ok

--- src/routes/askmfcheckqrcode/+server.js
+++ src/routes/askmfcheckqrcode/+server.js
@@ -48,7 +48,7 @@ export const POST = async ({request}) => {
 
 		const res = await axios.get(TELEGRAM_URI_FILE)
 
-		console.log(TELEGRAM_URI_FILE)
+		console.log(res)
 
 		botMessage = `https://api.telegram.org/file/bot${TELEGRAM_API_TOKEN_MFC_CHECK_QR}/${res?.result?.file_path}`
 
