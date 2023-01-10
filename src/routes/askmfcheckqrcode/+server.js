a177e4c7de43f42465b47e404288f30629e1ca9a
kolodiva <kolodiva@mail.ru>
2023-01-10 19:35:34 +0300

    ok

--- src/routes/askmfcheckqrcode/+server.js
+++ src/routes/askmfcheckqrcode/+server.js
@@ -67,7 +67,7 @@ export const POST = async ({request}) => {
 
 
 			Quagga.decodeSingle({
-			    src: url,
+			    src: image.bitmap.data,
 			    numOfWorkers: 0,  // Needs to be 0 when used within node
 			    inputStream: {
 			        size: 800  // restrict input-size to be 800px in width (long-side)
