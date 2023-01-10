ce85649580e6fef89764660abc0b653254b9a8f0
kolodiva <kolodiva@mail.ru>
2023-01-10 19:34:06 +0300

    ok

--- src/routes/askmfcheckqrcode/+server.js
+++ src/routes/askmfcheckqrcode/+server.js
@@ -64,7 +64,24 @@ export const POST = async ({request}) => {
       image.normalize()
       image.scale(2)
 
-			
+
+
+			Quagga.decodeSingle({
+			    src: url,
+			    numOfWorkers: 0,  // Needs to be 0 when used within node
+			    inputStream: {
+			        size: 800  // restrict input-size to be 800px in width (long-side)
+			    },
+			    decoder: {
+			        readers: ["ean_reader"] // List of active readers
+			    },
+			}, function(result) {
+			    if(result.codeResult) {
+			        console.log("result", result.codeResult.code);
+			    } else {
+			        console.log("not detected");
+			    }
+			});
 
 		 // const response = await axios.get(url, {
     	// responseType: 'arraybuffer'
