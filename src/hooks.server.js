4f56d78b489ca30680b8350aec7c5e73fb365391
kolodiva <kolodiva@mail.ru>
2023-03-28 20:25:24 +0300

    ok

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,11 +2,13 @@ export async function handle({ event, resolve }) {
 
 if (event.url.pathname.startsWith('/custom')) {
     const resp = new Response('custom response');
-    resp.headers.set('Access-Control-Allow-Origin', '*');
+    resp.headers.set('Access-Control-Allow-Origin', 'http://new.kolodiva.com');
     return resp;
   }
 
   const response = await resolve(event);
-  response.headers.set('Access-Control-Allow-Origin', '*');
+  //
+  response.headers.set('Access-Control-Allow-Origin', 'http://new.kolodiva.com');
+
   return response;
 }
