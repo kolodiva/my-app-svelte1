3f37fa4e5ae5738870d8d988a1c84c07577abde1
kolodiva <kolodiva@mail.ru>
2023-04-07 20:02:24 +0300

    ok

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -26,7 +26,7 @@ if (event.url.pathname.startsWith('/custom')) {
   //     response.headers.append('Access-Control-Allow-Origin', `*`);
   // }
 
-  response.headers.append('Access-Control-Allow-Methods', 'POST, GET');
+  response.headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   response.headers.append('Access-Control-Allow-Origin', `*`);
 
   return response;
