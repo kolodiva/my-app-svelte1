9f0b62e08fe13c5f2dca18a9624157fb409b1c4a
kolodiva <kolodiva@mail.ru>
2023-04-07 19:52:18 +0300

    ok

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -8,9 +8,23 @@ if (event.url.pathname.startsWith('/custom')) {
 
   const response = await resolve(event);
   //
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', 0);
+  // response.headers.set('Access-Control-Allow-Origin', '*');
+  // response.headers.set('Access-Control-Max-Age', 0);
 
+  // Apply CORS header for API routes
+  if (event.url.pathname.startsWith('/changeorder')) {
+      // Required for CORS to work
+      if(event.request.method === 'OPTIONS') {
+        return new Response(null, {
+          headers: {
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+            'Access-Control-Allow-Origin': '*',
+          }
+        });
+      }
+
+      response.headers.append('Access-Control-Allow-Origin', `*`);
+  }
 
   return response;
 }
