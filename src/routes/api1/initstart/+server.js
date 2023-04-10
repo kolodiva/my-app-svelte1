45199969b7b5168ded1c686948d5ef60885b5397
kolodiva <kolodiva@mail.ru>
2023-04-10 17:34:23 +0300

    ok

---
+++ src/routes/api1/initstart/+server.js
@@ -0,0 +1,18 @@
+import { query } from '$lib/server/getData.js'
+
+import { json, error } from '@sveltejs/kit';
+
+export const POST = async ({request}) => {
+
+	const msg = await request.json()
+
+	const sql = `SELECT initstart($1) AS "initStartResult";`
+
+	//console.log(msg)
+
+	const result = await query(sql, [ JSON.stringify(msg) ])
+
+	const {initStartResult} = result.rows[0]
+
+	return json(initStartResult)
+}
