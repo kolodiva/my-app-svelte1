4198b9127eaaafb56dba9c356431c7b090c70cfe
kolodiva <kolodiva@mail.ru>
2023-05-29 16:50:30 +0300

    ok

---
+++ src/routes/tgapi/mfcrevizorro/page.svelte
@@ -0,0 +1,29 @@
+
+<script>
+</script>
+
+<div class="flex align-items-center">
+  <span class="font-bold text-lg">Points:</span>
+  <div>
+    <span class="text-xl text-yellow-400"><i class="fas fa-star"></i></span>
+    <span class="text-xl text-yellow-200"><i class="fas fa-star"></i></span>
+    <span class="text-xl text-yellow-100"><i class="fas fa-star"></i></span>
+    <span class="text-xl text-yellow-50"><i class="fas fa-star"></i></span>
+  </div>
+</div>
+
+
+<style>
+.text-yellow-400 {
+  color: #ffc107;
+}
+.text-yellow-200 {
+  color: #ffab00;
+}
+.text-yellow-100 {
+  color: #ff9800;
+}
+.text-yellow-50 {
+  color: #ff8b00;
+}
+</style>
