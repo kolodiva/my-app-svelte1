87ba97891391d6d1c9df1e222f2b5604cd771ba1
kolodiva <kolodiva@mail.ru>
2023-05-30 16:14:05 +0300

    ok

--- src/routes/youtubechick/+server.js
+++ src/routes/youtubechick/+server.js
@@ -1,19 +1,42 @@
 import { require } from '$lib/server/createRequire.js'
 
-const stream = require('youtube-audio-stream')
+// const stream = require('youtube-audio-stream')
+const ytdl = require("ytdl-core");
 
 export const GET = async (event) => {
-	try {
-	    for await (const chunk of stream('https://youtu.be/8j5YBZ-c2uU')) {
-				const resp = new Response (chunk );
 
-				return resp
-	    }
+
+
+
+// await	ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
+// 	  .pipe(fs.createWriteStream('video.mp4'));
+
+	// const rangeHeader = event.request.headers || null
+	//
+	// 	console.log(rangeHeader)
+	//
+	//const streamURL = 'https://youtu.be/aqz-KE-bpKQ'
+	const streamURL = 'https://youtu.be/8iNfiNbG70Y?list=RD8iNfiNbG70Y&t=60'
+
+		try {
+
+			const resp = new Response()
+
+			//console.log(resp)
+
+			var stream = ytdl(streamURL, {
+		    quality: "highestaudio",
+		    filter: "audioonly",
+		  }).pipe(resp);
+
 	  } catch (err) {
 	    console.error(err)
-	    if (!res.headersSent) {
-	      res.writeHead(500)
-	      res.end('internal system error')
+
+			//const resp = new Response()
+
+	    if (!resp.headersSent) {
+	      // resp.writeHead(500)
+	      // resp.end('internal system error')
 	    }
 	  }
 }
