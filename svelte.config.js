6771034d782a830f83d68506a2a529fc9b74b31c
kolodiva <kolodiva@mail.ru>
2023-05-30 16:35:03 +0300

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-auto';
 //import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	onwarn: (warning, handler) => {
 	    if (warning.code.startsWith('a11y-')) {
