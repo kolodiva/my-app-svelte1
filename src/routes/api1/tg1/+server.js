abfdddf5606668cd5396da2c81a3e74a752e2d00
kolodiva <kolodiva@mail.ru>
2023-05-31 18:49:49 +0300

    ok

--- src/routes/api1/tg1/+server.js
+++ src/routes/api1/tg1/+server.js
@@ -4,39 +4,8 @@ import { json, error } from '@sveltejs/kit';
 
 export const POST = async ({request}) => {
 
-	const params = await request.json()
+	const params1 = await request.json()
 
-	if (params.oper === 'initData') {
-
-		return json({none: "none"});
-
-		const sql = `SELECT tg_mfcrevizorro_initstart($1) AS "initStartResult";`
-
-		//console.log(msg)
-
-		const result = await query(sql, [ JSON.stringify(params) ])
-
-		const {initStartResult} = result.rows[0]
-
-		return json(initStartResult)
-	}
-
-	if (params.oper === 'inActive') {
-
-		return json({none: "none"});
-
-		const sql = `SELECT tg_mfcrevizorro_inactive($1) AS "inactiveResult";`
-
-
-	//	console.log(JSON.stringify(params))
-
-		const result = await query(sql, [ JSON.stringify(params) ])
-
-		const {inactiveResult} = result.rows[0]
-
-		return json(inactiveResult)
-	}
-
-	return json({none: "none"});
+	return json({params1});
 
 }
