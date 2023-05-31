9922d80b2c90144431f038d6024ac87b604c298b
kolodiva <kolodiva@mail.ru>
2023-05-31 20:13:37 +0300

    ok

--- src/routes/api1/ddddd/+server.js
+++ src/routes/api1/ddddd/+server.js
@@ -2,12 +2,17 @@ import { query } from '$lib/server/getData.js'
 
 import { json, error } from '@sveltejs/kit';
 
-export const GET = async (event) => {
+export const POST = async ({request}) => {
 
-//export const POST = async ({request}) => {
+	const msg = await request.json()
 
-//return  json([{'msg':'йооооооооооооооооооооооооооо'}])
-const res = await json([{'msg':'йооооооооооооооооооооооооооо'}]);
-return res
+	const sql = `SELECT initstart($1) AS "initStartResult";`
 
+	//console.log(msg)
+
+	const result = await query(sql, [ JSON.stringify(msg) ])
+
+	const {initStartResult} = result.rows[0]
+
+	return json(initStartResult)
 }
