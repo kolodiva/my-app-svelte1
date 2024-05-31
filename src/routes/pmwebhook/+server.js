c08c46da1973df92607ba297b7646bd76e69a1d3
kolodiva <kolodiva@mail.ru>
2024-06-01 01:09:57 +0300

    ok

--- src/routes/pmwebhook/+server.js
+++ src/routes/pmwebhook/+server.js
@@ -2,40 +2,36 @@ import { query } from '$lib/server/getData.js'
 
 import { json, error } from '@sveltejs/kit';
 
-import { SECRET_KEY } from '$env/static/private';
+import { require } from '$lib/server/createRequire.js'
+const CryptoJS = require("crypto-js");
+
+import { SECRET_KEY, API_KEY_RAIF } from '$env/static/private';
 
 export const POST = async ({ request }) => {
 
 const params = await request.json()
 
-params.auth = request.headers.get('authorization');
-params.auth2 = request.headers.get('x-api-signature-sha256');
+const hash1 = request.headers.get('x-api-signature-sha256');
+
+const nonce = `${params.transaction.amount}|000003148440001-48440001|${params.transaction.orderId}|${params.transaction.status.value}|${params.transaction.status.date}`;
+
+const hash = CryptoJS.HmacSHA256(nonce, API_KEY_RAIF);
+const hash2 = CryptoJS.enc.Hex.stringify(hash);
+
+console.log(hash2);
+
+if (hash1 !== hash2) {
+		return json({'res':'niht ok Bro'}, {status: 500});
+	}
+
+	const sql = `SELECT raifpaymentscallback($1);`
+
+	const result = await query(sql, [ params ])
+
+	const data = result.rows[0];
+
+	console.log(data);
 
 
-console.log(params);
-//
- //console.log(request.headers.get('authorization'));
-//console.log(request);
-
-// request.headers.value.map((pos) => {
-// 	console.log(pos)
-// })
-
-
-//console.log(params.data || params.data.id2 || params.data.id2 === SECRET_KEY);
-
-	// if (params.data && params.data.id2 && params.data.id2 !== SECRET_KEY) {
-	// 		//console.log(params.data.id2 === SECRET_KEY);
-	// 		// console.log(params.data || params.data.id2 || params.data.id2 === SECRET_KEY);
-	// 		return json({'res':'noBro'});
-	// }
-
-	// const sql = `SELECT raifpayments($1) AS "raifpaymentsResult";`
-	//
-	// const result = await query(sql, [ params.data ]);
-	//
-	// const {raifpaymentsResult} = result.rows[0];
-
-	//return json(params, {status: 200});
-	return json({'res':'ok'}, {status: 200});
+return json({'res':'ok'}, {status: 200});
 }
