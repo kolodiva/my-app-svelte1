312d253989ec719dd63b1c53a30afd99dd3fcb17
kolodiva <kolodiva@mail.ru>
2024-06-08 23:25:59 +0300

    ok

---
+++ src/lib/server/createOrange.js
@@ -0,0 +1,124 @@
+import { require } from '$lib/server/createRequire.js'
+
+const { OrangeData, Order } = require('node-orangedata');
+const { OrangeDataError } 	= require('node-orangedata/lib/errors');
+
+const fs 		= require('fs');
+const path 	= require('path');
+
+function getOrderObj() {
+
+  const pathtokey = path.resolve('src/lib/server/keys');
+
+  const cert         = fs.readFileSync( path.join(pathtokey, 'client.crt') );
+  const key          = fs.readFileSync( path.join(pathtokey, 'client.key') );
+  const passphrase   = '1234';
+  const ca           = fs.readFileSync(path.join( path.join(pathtokey, 'cacert.pem' )));
+  const privateKey   = fs.readFileSync(path.join( path.join(pathtokey, 'private_key.pem')));
+
+  const apiUrl       = 'https://apip.orangedata.ru:12001/api/v2';
+
+  const agent = new OrangeData({ apiUrl, cert, key, passphrase, ca, privateKey });
+
+  return agent;
+
+}
+
+export async function createCheck(params) {
+
+  // console.log(params.content.positions);
+  // console.log(params.content.checkClose.payments);
+
+  const agent = getOrderObj();
+
+  //console.log(params.content.positions);
+
+  try {
+
+    const order = new Order({
+		  id: params.id,
+		  inn: params.inn,
+			key: params.key,
+		  type: params.content.type, // Приход 1
+		  taxationSystem: params.content.checkClose.taxationSystem, // Общая 0
+			customerContact: params.content.customerContact,
+		});
+
+    //console.log('11111111111111111111111')
+
+    params.content.positions.forEach((item, i) => {
+      order.addPosition(item)
+    });
+
+    params.content.checkClose.payments.forEach((item, i) => {
+      order.addPayment(item);
+    });
+    //
+    //console.log(order)
+
+
+    //
+		await agent.sendOrder(order);
+
+	} catch (error) {
+
+    //console.log(error)
+    let msgError;
+
+    if (error instanceof OrangeDataError) {
+		    // OrangeData errors contains additional info in `errors` property of type Array
+		      //console.log(error.message, error.errors);
+          msgError = error.message + " " + (error.errors ? error.errors : "");
+          //console.log(msgError);
+	       } else {
+           msgError = error;
+         }
+	       return {error: true, msg: msgError} // general errors handling
+	}
+
+  return {error: false, msg: 'Чек ККМ создан'};
+}
+
+/////////
+export async function statusCheck(params) {
+
+  //console.log(params);
+
+  const agent = getOrderObj();
+
+  //let status;
+
+	//await sleep(400);
+
+	try {
+
+    const status = await agent.getOrderStatus(`${params.inn}`, `${params.id}`);
+
+
+
+    if (status) {
+		// Документ успешно обработан, status содержит данные документа
+    //console.log(status);
+      return status;
+    } else {
+    // Документ создан и добавлен в очередь на обработку, но еще не обработан
+		//console.log(status);
+    return {error: true}
+    }
+} catch (error) {
+
+  let msgError;
+
+  if (error instanceof OrangeDataError) {
+    // OrangeData errors contains additional info in `errors` property of type Array
+    //console.log(error.message + ' ' + error.errors);
+    msgError = error.message + " " + (error.errors ? error.errors : "");
+
+  } else {
+    msgError = error;
+  }
+  // general errors handling
+  return {error: true, msg: msgError}
+}
+
+}
--- src/routes/pmwebhook/+server.js
+++ src/routes/pmwebhook/+server.js
@@ -2,11 +2,41 @@ import { query } from '$lib/server/getData.js'
 
 import { json, error } from '@sveltejs/kit';
 
+import { createCheck, statusCheck } from '$lib/server/createOrange.js'
+
 import { require } from '$lib/server/createRequire.js'
+
 const CryptoJS = require("crypto-js");
 
 import { SECRET_KEY, API_KEY_RAIF } from '$env/static/private';
 
+function statusCheckPromise(params) {
+
+  return new Promise((resolve, reject) => {
+
+    setTimeout(async() => {
+
+      const status = await statusCheck(params);
+
+      //console.log(status);
+
+      if (status && status.error) {
+
+        const sql = `update raifpayments set check_status='ERROR', checkcallback=($2)  where guid=($1) AND (check_status = 'ERROR' OR check_status is null OR check_status = '') ;`
+
+      	query(sql, [ params.id, status ])
+
+      } else {
+
+        const sql = `update raifpayments set check_status='"SUCCESS"', checkcallback=($2) where guid=($1) ;`
+
+        query(sql, [ params.id, status ])
+      }
+    }, 5*1000)
+  });
+}
+
+/////////////////////////////////////////////
 export const POST = async ({ request }) => {
 
 const params = await request.json()
@@ -18,12 +48,13 @@ const nonce = `${params.transaction.amount}|000003148440001-48440001|${params.tr
 const hash = CryptoJS.HmacSHA256(nonce, API_KEY_RAIF);
 const hash2 = CryptoJS.enc.Hex.stringify(hash);
 
-// console.log(hash2);
+ //console.log(hash2);
 
 if (hash1 !== hash2) {
 		return json({'res':'niht ok Bro'}, {status: 500});
 	}
 
+	//регистрруем Оплату и берем в ответе табл чакст ьдля чека
 	const sql = `SELECT raifpaymentscallback($1);`
 
 	const result = await query(sql, [ params ])
@@ -33,9 +64,35 @@ if (hash1 !== hash2) {
 	// console.log(data.raifpaymentscallback.status);
 
 	if (data.raifpaymentscallback.status === 'Ok') {
-		
-			return json({'res':'ok'}, {status: 200});
+
+		//
+		const result = await createCheck(data.raifpaymentscallback.check);
+
+	  if (result.error) {
+
+	    //если ОШибка при создании чека записываем ее в базу по GUID
+	    //console.log(result);
+	    const sql = `update raifpayments set check_status='ERROR', checkcallback=($2) where guid=($1) AND (check_status = 'ERROR' OR check_status is null OR check_status = '') ;`
+
+	  	await query(sql, [ params.id, result ])
+
+	  	//const data = resultsql.rows[0];
+	    //возвращаем райфу 500 считаем что чек не создался по тех причинам и надо его пересоздать но инициатором будет райф
+	    if (result.msg.trim() == 'Чек с данным идентификатором уже был создан в системе') {
+	    } else {
+	        return json( {'res' : 'niht OK', result} , {status: 500});
+	    }
+	  }
+
+		//через 5 сек обновим статус
+	  statusCheckPromise(data.raifpaymentscallback.check);
+
+		//
+		return json({'res': data.raifpaymentscallback}, {status: 200});
+
 	} else {
-		return json({'res':'niht ok Bro'}, {status: 500});
+		//return json({'res':'niht ok Bro'}, {status: 500});
+		return json({'res': data.raifpaymentscallback}, {status: 500});
+
 	}
 }
