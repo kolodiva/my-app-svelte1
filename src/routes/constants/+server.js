48b5bb05ac2ad8b568e745c203f2fe701f651d6a
kolodiva <kolodiva@mail.ru>
2025-03-17 22:35:01 +0300

    ok

--- src/routes/constants/+server.js
+++ src/routes/constants/+server.js
@@ -2,9 +2,16 @@ import { query } from '$lib/server/getData.js'
 
 import { json, error } from '@sveltejs/kit';
 
+import { SECRET_KEY1 } from '$env/static/private'
+
 export const GET = async (event) => {
 
-	const params = await request.json()
+	const safe_id = event.url.searchParams.get('id')
+
+	if (SECRET_KEY1 !== safe_id) {
+		return json([])
+	}
+
 
 			const sql = `SELECT get_constants() AS "getConstants";`
 
