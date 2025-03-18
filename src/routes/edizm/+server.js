2088ba43ce73cbd221fbf996b36f93d7028a9968
kolodiva <kolodiva@mail.ru>
2025-03-18 12:55:18 +0300

    ok

---
+++ src/routes/edizm/+server.js
@@ -0,0 +1,24 @@
+import { query } from '$lib/server/getData.js'
+
+import { json, error } from '@sveltejs/kit';
+
+import { SECRET_KEY1 } from '$env/static/private'
+
+export const GET = async (event) => {
+
+	const safe_id = event.url.searchParams.get('id')
+
+	if (SECRET_KEY1 !== safe_id) {
+		return json([])
+	}
+
+	const guid = event.url.searchParams.get('guid')
+
+			const sql = `SELECT get_edizm($1) AS "getEdizm";`
+
+			const result = await query(sql, [guid])
+
+			const {getEdizm} = result.rows[0]
+
+			return json(getEdizm)
+}
